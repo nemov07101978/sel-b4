e1350b7ba4162ab1229694d1a760c915824b44b0
nemov07101978 <action.tester1@gmail.com>
2019-04-19 08:08:55 +0300

    задание 3 - логин в http://localhost/litecart/admin/
    задание 3 - логин в http://localhost/litecart/admin/

---
+++ selenium_tests/javaex/src/test/java/sel/b4/login_litecart_admin.java
@@ -0,0 +1,37 @@
+package sel.b4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class login_litecart_admin {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    @Test
+    public void login_admin() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop () {
+        driver.quit();
+        driver = null;
+    }
+}
